62aafa46ab3d40b6b962c1bf5c27aba4ea57f79b
titilayo <titilayotijani15@gmail.com>
2023-06-05 16:00:17 -0700

    hello, write retry 1

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import sys
-def print_to_stderr(*a):
-    # Here a is the array holding the objects 
-    # passed as the argument of the function 
-    print(*a, file = sys.stderr)
-    print_to_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19")
-    sys.exit(1) 
+def print_to_stderr(*a): 
+  
+    print(*a, file = sys.stderr) 
+  
+print_to_stderr("and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.exit(1) 
